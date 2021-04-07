c01eec268610b583af0da3e5ed86997a829bc9e9
UMuryn <67969825+UMuryn@users.noreply.github.com>
2021-04-07 15:58:15 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "access_key" {
+  description = "The access_key for aws"
+  type        = string
+}
+variable "secret_key" {
+  description = "The secret_key for aws"
+  type        = string
+}
+variable "region" {
+  description = "The region for aws"
+  type        = string
+}
+variable "OAuthToken" {
+  description = "The OAuthToken for github"
+  type        = string
+}
