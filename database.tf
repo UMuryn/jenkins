2453543c6048e532fb5c3b282eb5e4b6c26523ad
UMuryn <67969825+UMuryn@users.noreply.github.com>
2021-04-07 15:57:53 +0300

    Create database.tf

---
+++ database.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+access_key = var.access_key
+secret_key = var.secret_key
+  region = var.region
+}
+resource "aws_db_instance" "lavagna" {
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "lavagna"
+ allocated_storage     = 50
+  max_allocated_storage = 100
+  # Set the secrets from variables
+  username             = var.username
+  password             = var.password
+}
