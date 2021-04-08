6a8337c07469ef0d08ef0ae05f836cc957a24411
UMuryn <67969825+UMuryn@users.noreply.github.com>
2021-04-08 15:00:01 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,25 +1,26 @@
 variable "access_key" {
   description = "The access_key for aws"
   type        = string
+  default     = "access_key"
 }
 variable "secret_key" {
   description = "The secret_key for aws"
   type        = string
+  default     = "secret_key"
 }
 variable "region" {
   description = "The region for aws"
   type        = string
-}
-variable "OAuthToken" {
-  description = "The OAuthToken for github"
-  type        = string
+  default     = "region"
 }
 variable "username" {
   description = "The username for db"
   type        = string
+  default     = "user"
 }
 
 variable "password" {
   description = "The password for db"
   type        = string
+  default     = "password"
 }
