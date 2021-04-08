58bf6d1fd427a4e47ac4b230997461205ffad2fc
UMuryn <67969825+UMuryn@users.noreply.github.com>
2021-04-08 14:54:26 +0300

    Update database.tf

--- database.tf
+++ database.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 #access_key = var.access_key
 #secret_key = var.secret_key
-  region = var.region
+#  region = var.region
 }
 resource "aws_db_instance" "lavagna" {
   engine               = "mysql"
