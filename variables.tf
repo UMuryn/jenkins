b3f83eb5e84db74cd7d73efaeb2d4876e5467f67
UMuryn <67969825+UMuryn@users.noreply.github.com>
2021-04-08 16:10:04 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,27 +8,7 @@ terraform {
 }
 variable "access_key" {
   description = "The access_key for aws"
-  type        = string
+  type        = 'string'
   default     = "access_key"
 }
-variable "secret_key" {
-  description = "The secret_key for aws"
-  type        = string
-  default     = "secret_key"
-}
-variable "region" {
-  description = "The region for aws"
-  type        = string
-  default     = "region"
-}
-variable "username" {
-  description = "The username for db"
-  type        = string
-  default     = "user"
-}
 
-variable "password" {
-  description = "The password for db"
-  type        = string
-  default     = "password"
-}
