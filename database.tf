6a6f1b6d3e4c7694fdc6bfc6ee7044c2a8d9b650
UMuryn <67969825+UMuryn@users.noreply.github.com>
2021-04-08 15:53:52 +0300

    Update database.tf

--- database.tf
+++ database.tf
@@ -1,5 +1,10 @@
 terraform {
-  required_version = ">= 0.14"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
 }
 provider "aws" {
 access_key = var.access_key
