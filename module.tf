967c9e01d46954d1088810f093e6e12b41793c5e
UMuryn <67969825+UMuryn@users.noreply.github.com>
2021-04-08 16:03:19 +0300

    Update module.tf

--- module.tf
+++ module.tf
@@ -1,4 +1,5 @@
 module "ec2-lb-sg" {
+  version = "~> 1.66.0"
   source          = "git@github.com:DChekirda-Trainee-Team/Terraform-Modules-Uliana.git//modules/ec2-lb-sg?ref=modules/ec2-lb-sg-0.2.0"
   access_key      = var.access_key
   secret_key      = var.secret_key
