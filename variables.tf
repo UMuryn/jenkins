69f693fb1d5d18dff9a5e6f22789287f888b9618
UMuryn <67969825+UMuryn@users.noreply.github.com>
2021-04-08 16:12:27 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,4 +11,24 @@ variable "access_key" {
   type        = "string"
   default     = "access_key"
 }
+variable "secret_key" {
+  description = "The secret_key for aws"
+  type        = "string"
+  default     = "secret_key"
+}
+variable "region" {
+  description = "The region for aws"
+  type        = "string"
+  default     = "region"
+}
+variable "username" {
+  description = "The username for db"
+  type        = "string"
+  default     = "user"
+}
 
+variable "password" {
+  description = "The password for db"
+  type        = "string"
+  default     = "password"
+}
